e179912036eddb7999bbbdfc36c041aee1316e49
Ayush-Yadav <ayushyadav2006k@gmail.com>
2026-06-01 12:28:47 +0530

    This determines the best time to buy and sell stocks

---
+++ 2-ARRAYS/Buy_and_sell_STOCKS.java
@@ -0,0 +1,18 @@
+public class Buy_and_sell_STOCKS {
+    public int maxProfit(int[] prices) {
+    int left=0;
+    int right=1;
+    int maxprofit=0;
+    while (right< prices.length){
+        if (prices[left]<prices[right]){
+            int profit= prices[right]-prices[left];
+            maxprofit=Math.max(profit,maxprofit);
+        }
+        else{
+            left=right;
+        }
+        right++;
+    }
+    return maxprofit;
+    }
+}
