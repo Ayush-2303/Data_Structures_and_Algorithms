a4ed236562d9257d22126681b932513162c36e4d
Ayush-Yadav <ayushyadav2006k@gmail.com>
2026-06-02 15:20:14 +0530

    This is the code to check if the code contains duplicates with using hashmap.

---
+++ 2-ARRAYS/Contains_duplicates_using_hashmap.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Contains_duplicates_using_hashmap {
+        public boolean containsDuplicate(int[] nums) {
+            HashSet<Integer> Seen = new HashSet<>();
+            for ( int val : nums){
+                if (Seen.contains(val)){
+                    return true;
+                }
+                else{
+                    Seen.add(val);
+                }
+            }return false;
+        }
+    }
