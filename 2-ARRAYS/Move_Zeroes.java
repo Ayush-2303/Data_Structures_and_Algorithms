71cef43a6d33afd8d11f71e806b0697756bab7ab
Ayush-Yadav <ayushyadav2006k@gmail.com>
2026-06-02 13:08:19 +0530

    This tells us the working of a moving zeroes type

--- 2-ARRAYS/Move_Zeroes.java
+++ 2-ARRAYS/Move_Zeroes.java
@@ -1,7 +1,7 @@
 public class Move_Zeroes {
     public void moveZeroes(int[] nums) {
         int i = 0;
-        int temp=0;
+        int temp;
         for (int j = 0 ; j < nums.length; j++){
             if (nums[j]!=0){
                 temp=nums[i];
