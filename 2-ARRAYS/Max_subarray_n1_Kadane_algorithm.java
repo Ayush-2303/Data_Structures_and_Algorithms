80da7745968d061e7abf293d27b45fb9c4da6a54
Ayush-Yadav <ayushyadav2006k@gmail.com>
2026-06-02 11:09:09 +0530

    This is Kadane algorithm which has a time complexity of O(n)

--- 2-ARRAYS/Max_subarray_n1_Kadane_algorithm.java
+++ 2-ARRAYS/Max_subarray_n1_Kadane_algorithm.java
@@ -1,17 +1,14 @@
 public class Max_subarray_n1_Kadane_algorithm {
     public int maxSubArray(int[] nums) {
-        
+        //Initialize to MIN_VALUE to safely handle negative number arrays
         int smax=0;
-        int maxi=0;
+        int maxi=Integer.MIN_VALUE;
         for (int i=0; i< nums.length; i++){
             smax=smax+nums[i];
-            //if sum of two alternate numbers is -ve we will change current sum to 0
+            maxi= Math.max(maxi,smax);
             if( smax<0){
                 smax=0;
             }
-            maxi= Math.max(maxi,smax);
-            
-            
         } return maxi;
         
     }
