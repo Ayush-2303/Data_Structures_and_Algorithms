bca2ae52ca84c75ac614442f52df42a9c4cd8ba0
Ayush-Yadav <ayushyadav2006k@gmail.com>
2026-06-05 12:19:26 +0530

    This is to check the string is a palindrome or not (it should ignore the commas and white spaces) go till half use left and right check it digit or number is absent compare both the string

---
+++ 3-STRINGS/Valid_Palindrome.java
@@ -0,0 +1,24 @@
+public class Valid_Palindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() -1;
+        //left is always less than right 
+        while(left<right){
+                //skip and increment from the left if  the character is not a number or digit
+            while( left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            //skip and decrement from the right if the character is not a number or digit
+            while( left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if ( Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            right --;
+            left++;
+            
+        }return true;
+    }
+}
